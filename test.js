c8b1079202a1c7f269a9316536eec0b054a1c916
solzimer <solzimer@gmail.com>
2017-05-03 15:07:04 +0200

    test

---
+++ test.js
@@ -0,0 +1,4 @@
+const parser = require("./parser.js");
+
+console.log(parser("<34>Oct 11 22:14:15 mymachine su: 'su root' failed for lonvick on /dev/pts/8"));
+console.log(parser("<34>1 2003-10-11T22:14:15.003Z mymachine.example.com su - ID47 - BOM'su root' failed for lonvick on /dev/pts/8"));
