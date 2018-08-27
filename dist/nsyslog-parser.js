060547fcaf5f4ddb62a64b9b6a7eb5f2e3679772
solzimer <solzimer@gmail.com>
2018-08-27 11:51:58 +0200

    Header bug

--- dist/nsyslog-parser.js
+++ dist/nsyslog-parser.js
@@ -1,18 +1,20 @@
 "use strict";
 
-(function e(t, n, r) {
-	function s(o, u) {
-		if (!n[o]) {
-			if (!t[o]) {
-				var a = typeof require == "function" && require;if (!u && a) return a(o, !0);if (i) return i(o, !0);var f = new Error("Cannot find module '" + o + "'");throw f.code = "MODULE_NOT_FOUND", f;
-			}var l = n[o] = { exports: {} };t[o][0].call(l.exports, function (e) {
-				var n = t[o][1][e];return s(n ? n : e);
-			}, l, l.exports, e, t, n, r);
-		}return n[o].exports;
-	}var i = typeof require == "function" && require;for (var o = 0; o < r.length; o++) {
-		s(r[o]);
-	}return s;
-})({ 1: [function (require, module, exports) {
+(function () {
+	function r(e, n, t) {
+		function o(i, f) {
+			if (!n[i]) {
+				if (!e[i]) {
+					var c = "function" == typeof require && require;if (!f && c) return c(i, !0);if (u) return u(i, !0);var a = new Error("Cannot find module '" + i + "'");throw a.code = "MODULE_NOT_FOUND", a;
+				}var p = n[i] = { exports: {} };e[i][0].call(p.exports, function (r) {
+					var n = e[i][1][r];return o(n || r);
+				}, p, p.exports, r, e, n, t);
+			}return n[i].exports;
+		}for (var u = "function" == typeof require && require, i = 0; i < t.length; i++) {
+			o(t[i]);
+		}return o;
+	}return r;
+})()({ 1: [function (require, module, exports) {
 		"use strict";
 
 		(function ($) {
@@ -113,11 +115,11 @@
 			"pri": /^<\d+>/,
 			"prinmr": /^\d+ /,
 			"prival": /<(\d+)>/,
-			"month": /^[a-zA-Z]{3} /,
+			"month": /^[A-Z][a-z]{2} /,
 			"day": /^\d{1,2} /,
 			"time": /^\d+:\d+:\d+ /,
 			"ts": /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\S+ /,
-			"invalid": /[^a-zA-Z0-9\.\$\-_#%\/\[\]]/,
+			"invalid": /[^a-zA-Z0-9\.\$\-_#%\/\[\]\(\)]/,
 			"sdata": /\[(\S+)( [^\=]+\=\"[^\"]+\")+\]/g,
 			"cef": /^CEF:\d+/
 		};
